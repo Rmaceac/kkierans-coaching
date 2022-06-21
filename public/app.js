00786fb4691e27c9713c55b121a7631fb63f822c
Rmaceac <ryan.maceachern@gmail.com>
2022-06-21 13:12:03 -0700

    fix for firebase initialization problem. add onclick event listeners for sign in button

--- public/app.js
+++ public/app.js
@@ -1,3 +1,19 @@
 // import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 // import { getAuth } from "firebase/auth";
+
+const auth = firebase.auth();
+
+const whenSignedIn = document.getElementById('whenSignedIn');
+const whenSignedOut = document.getElementById('whenSignedOut');
+
+const signInBtn = document.getElementById('signInBtn');
+const signOutBtn = document.getElementById('signOutBtn');
+
+const userDetails = document.getElementById('userDetails');
+
+const provider = new firebase.auth.GoogleAuthProvider();
+
+signInBtn.onclick = () => auth.signInWithPopup(provider);
+
+signOutBtn.onclick = () => auth.signOut();
