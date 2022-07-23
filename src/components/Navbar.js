55b7ad39a1f3be0841deeb2a1b4d7648926f575b
Rmaceac <ryan.maceachern@gmail.com>
2022-07-23 00:16:29 -0700

    add list items and endpoints to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,6 +2,11 @@ import { useState } from 'react';
 import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+  
   return (
     <>
       <nav className="navbar">
@@ -9,6 +14,37 @@ const Navbar = () => {
           <Link to="/" className="navbar-logo">
             KK <i className="fa-solid fa-lemon" />
           </Link>
+          <div className="menu-icon" onClick={handleClick}>
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/about" className="nav-links" onClick={closeMobileMenu}>
+                Meet Kat
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/testimonials" className="nav-links" onClick={closeMobileMenu}>
+                Testimonials
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/blog" className="nav-links" onClick={closeMobileMenu}>
+                Blog
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/uwc" className="nav-links" onClick={closeMobileMenu}>
+                Unbound Women's Collective
+              </Link>
+            </li>
+
+          </ul>
         </div>
       </nav>
     </>
