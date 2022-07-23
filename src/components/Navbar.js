1c585eedb74c5192896c8eb86864a567daae7ba7
Rmaceac <ryan.maceachern@gmail.com>
2022-07-23 01:14:50 -0700

    setup button for mobile responsiveness

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,9 +3,20 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   const [click, setClick] = useState(false);
+  const [button, setButton] = useState(true);
 
   const handleClick = () => setClick(!click);
   const closeMobileMenu = () => setClick(false);
+
+  const showButton = () => {
+    if (window.innerwidth <= 960) {
+      setButton(false);
+    } else {
+      setButton(true);
+    }
+  };
+
+  window.addEventListener('resize', showButton)
   
   return (
     <>
