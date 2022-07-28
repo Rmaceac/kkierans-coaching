e2b6c1f859fb633bca68be33e8c6901638e982a8
Rmaceac <ryan.maceachern@gmail.com>
2022-07-27 18:25:55 -0700

    linting. add Home page to Router

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Video from './components/Video';
+import Home from './pages/Home'
 import About from './pages/About';
 
 function App() {
   return (
     <>
-    <Router>
-      <Navbar />
-      <Video />
-      <Routes>
-        <Route path="/" exact />
-        <Route path="/about" component={About} />
-        <Route path="/testimonials" />
-        <Route path="/blog" />
-        <Route path="/uwc" />
-      </Routes>
-    </Router>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" exact component={Home}/>
+          <Route path="/about" component={About} />
+          <Route path="/testimonials" />
+          <Route path="/blog" />
+          <Route path="/uwc" />
+        </Routes>
+      </Router>
     </>
   );
 }
