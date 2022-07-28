4bdde00c4cdb8be4c2d684f14dbb81f57cec5600
Rmaceac <ryan.maceachern@gmail.com>
2022-07-27 18:29:11 -0700

    add headings to Home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,9 @@
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div>
+      <h1>Unbound & Held</h1>
+      <h6>Helping you overcome stress & anxiety, naturally.</h6>
+    </div>
   )
 }
