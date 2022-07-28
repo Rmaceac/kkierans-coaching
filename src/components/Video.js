4956126987c9fedf84eded0b31c9b26dc22fed67
Rmaceac <ryan.maceachern@gmail.com>
2022-07-27 17:21:52 -0700

    video src filepath adjusted

--- src/components/Video.js
+++ src/components/Video.js
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from 'react';
+import './Video.css';
 
 export default function Video() {
   return (
     <div className='video-container'>
-      <video src='/../../public/videos/vid-1.mp4' autoPlay loop muted></video>
-      <h1>Unbound and Held</h1>
-      <p>Helping you overcome stress & anxiety, naturally.</p>
+      <video src='/videos/vid-1.mp4' autoPlay loop muted></video>
+      <h1>Meet Kat</h1>
     </div>
   )
 }
