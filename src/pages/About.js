d1fb2e680186eb4fe717c84642c2e8c9593d271e
Rmaceac <ryan.maceachern@gmail.com>
2022-07-28 15:41:30 -0700

    create About.css file. adjust video container to hold video

--- src/pages/About.js
+++ src/pages/About.js
@@ -5,7 +5,10 @@ export default function About() {
   return (
     <>
       <Video src='/videos/vid-1.mp4' title='Meet Kat' />
-      <div>About Kat</div>
+      <section className='section-dark'>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sodales eros metus, at convallis orci cursus rhoncus. </p>
+        <p>Proin at mauris sit amet neque congue luctus. Maecenas non risus nibh. </p>
+      </section>
     </>
   )
 }
