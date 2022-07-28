2f9e19de6a2b5dcad3f2a3645edea50fa4e404ce
Rmaceac <ryan.maceachern@gmail.com>
2022-07-27 23:15:39 -0700

    banner headings now render using aray.map

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -7,12 +7,9 @@ export default function Banner({ headings }) {
         <hr />
       </div>
       <div className="headings-container">
-        <h2>HEADING 1</h2>
-        <h2>HEADING 2</h2>
-        <h2>HEADING 3</h2>
-        {/* {headings.map((h) => {
-          <h2>{h}</h2>
-        })} */}
+        {headings.map((h) => {
+          return <h2>{h}</h2>
+        })}
       </div>
       <div className='banner-edge'>
         <hr />
