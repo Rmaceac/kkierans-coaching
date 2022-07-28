98300f221fa2c7ba990c92210a43f8ddeb0da42c
Rmaceac <ryan.maceachern@gmail.com>
2022-07-28 16:19:24 -0700

    adjust styling for headings below video

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,16 @@
-import Video from '../components/Video'
-import '../App.css'
+import Video from '../components/Video';
+import '../App.css';
+import './About.css';
 
 export default function About() {
   return (
     <>
       <Video src='/videos/vid-1.mp4' title='Meet Kat' />
       <section className='section-dark'>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris sodales eros metus, at convallis orci cursus rhoncus. </p>
-        <p>Proin at mauris sit amet neque congue luctus. Maecenas non risus nibh. </p>
+        <div className='heading-container'>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </p>
+          <p>Proin at mauris sit amet neque congue luctus. </p>
+        </div>
       </section>
     </>
   )
