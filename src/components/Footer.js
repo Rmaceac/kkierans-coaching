478fc8d8c9223112ee71c1e002010f67674e6a25
Rmaceac <ryan.maceachern@gmail.com>
2022-08-01 08:21:57 -0700

    add divs for footer links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,6 +14,19 @@ const Footer = () => {
             <i className="fa-brands fa-instagram"/>
           </a>
         </div>
+
+        <div className='footer-links'>
+          <div className='footer-links-left'>
+            <h4>Free Clarity Call</h4>
+            <h4>Unbound Women's Collective</h4>
+            <h4>Contact</h4>
+          </div>
+          <div className='footer-links-right'>
+            <h4>Link 4</h4>
+            <h4>Link 5</h4>
+            <h4>Link 6</h4>
+          </div>
+        </div>
       </section>
     </div>
   )
