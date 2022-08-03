c28b4cb11dd8e25ed1632ed27edf0f6845f4b47c
Ryan <ryan.maceachern@gmail.com>
2022-08-03 15:35:00 -0700

    add Kat's acuity ID, style acuity scheduler

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -3,7 +3,14 @@ export default function Schedule() {
   return (
     <div>
       <section className='acuity-block-container'>
-        <iframe title='acuity-scheduler' src='https://example.acuityscheduling.com' width='100%' height='800' frameBorder='0'></iframe>
+        <iframe 
+          className='acuity-frame'
+          src='https://app.acuityscheduling.com/schedule.php?owner=22654704'
+          width='100%'
+          frameborder='0'
+          title='Schedule Appointment'
+          data-iframe-translate=''
+        ></iframe>
         <script src='https://embed.acuityscheduling.com/js/embed.js' type='text/javascript'></script>
       </section>
     </div>
